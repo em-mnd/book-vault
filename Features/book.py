42d8e3b895c15ca3d3824d0c6eadce78d9d86da2
Cladvyr <167028773+em-mnd@users.noreply.github.com>
2026-07-25 23:26:10 +0200

    Updated files
    Added classes and functions to files, figuring out how to organize my classes and how to use them, call them with menu, adding user input.

--- Features/book.py
+++ Features/book.py
@@ -4,10 +4,38 @@ class Book():
     def __init__(self, name, author, description = ""):
         self.name = name
         self.author = author
-        self.description = ""
+        self.description = description
+        self.rating = None
+        self.is_read = False
     
-    def rate_a_book(self):
-        pass
-        
+    def rate(self, rating):
+        if not 0 <= rating <= 5:
+            raise ValueError("Rating must be between 0 and 5.")
+        self.rating = rating
+
     def mark_as_read(self):
+        self.is_read = True
+
+
+class BookVault():
+    def __init__(self):
+        self.books = []
+
+    def add_book(self):
+        choice = input('Do you want to add a book ? (y/n): ')
+        if not choice in ('y', 'n'):
+            raise ValueError('Please choice between (y/n).')
+        elif choice == 'y':
+            Book()
+        elif choice == 'n':
+            print('Returning to main menu...')
+        self.books.append(self.books)
+    
+    def remove_book(self):
+        pass
+    
+    def view_books(self):
+        pass
+    
+    def search_books(self):
         pass
--- Features/main.py
+++ Features/main.py
@@ -1,1 +1,8 @@
 # This file manages the main menu and calls the right function when needed
+
+def main_menu():
+    main_choices = input('Welcome to the Book Vault. Please choose an option: 1. Books\n2. Exit') #I don't have ideas for options
+    if not main_choices in ('1', '2'):
+        raise ValueError('Please choose between option (1) or (2).')
+
+# Trying to figure out if my classes should have their own menu since they (will) have multiple methods.
