f53d91fead99296226404c12f74ab3edd9e7eeaa
Riri <167028773+em-mnd@users.noreply.github.com>
2026-07-25 19:48:42 +0200

    Update book.py
    Added classes

--- Features/book.py
+++ Features/book.py
@@ -1,1 +1,13 @@
 # This file manages individual books
+
+class Book():
+    def __init__(self, name, author, description = ""):
+        self.name = name
+        self.author = author
+        self.description = ""
+    
+    def rate_a_book(self):
+        pass
+        
+    def mark_as_read(self):
+        pass
